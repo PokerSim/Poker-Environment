a9944de2684bac15e8ffa845243f06b8adbb876d
rcgo234 <160293661+rcgo234@users.noreply.github.com>
2025-02-13 14:59:44 -0500

    Update Poker_env.py

--- Poker_env.py
+++ Poker_env.py
@@ -11,8 +11,10 @@ class Poker_env:
         self.dealer = 0 # Possibly use a pointer
         self.deck = self.initialize_deck()
         self.community_cards = []
+        self.burn_cards = []
         self.action_pointer = 0
         self.round_stage = "pre-flop"
+        self.current_bet = 0
 
     def initialize_deck(self):
         suits = ["s", "h", "d", "c"]
@@ -26,4 +28,30 @@ class Poker_env:
                 if player.active:
                     player.receive_card(self.deck.pop())
 
+    def post_blinds(self):
+        small_blind_player = self.get_next_active_player(self.dealer+1)
+        big_blind_player = self.get_next_active_player(self.dealer+2)
+        small_blind_player.chips -= self.small_blind
+        big_blind_player.chips -= self.big_blind
+        self.pot += self.small_blind + self.big_blind
+
+    def betting_round(self):
+        pass
     
+    def deal_board(self):
+        if self.round_stage == "flop":
+            self.burn_cards.append(self.deck.pop())
+            for i in range(3):
+                self.community_cards.append(self.deck.pop())
+            
+        elif self.round_stage == "turn":
+            self.burn_cards.append(self.deck.pop())
+            self.community_cards.append(self.deck.pop())
+
+        elif self.round_stage == "river":
+            self.burn_cards.append(self.deck.pop())
+            self.community_cards.append(self.deck.pop())
+    def get_next_active_player(self, dealer):
+        pass
+
+
